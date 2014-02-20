fa00a1d944511a1b995a83d8017bc9d46908d554
pfefferle <pfefferle@gmail.com>
2014-02-20 18:25:42 +0100

    new image sizes

--- zenpress/functions.php
+++ zenpress/functions.php
@@ -1,7 +1,18 @@
 <?php
-
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if ( ! isset( $content_width ) )
+  $content_width = 900; /* pixels */
 
 function zenpress_setup() {
+  // This theme uses post thumbnails
+  add_theme_support( 'post-thumbnails' );
+  set_post_thumbnail_size( 900, 1288 ); // Unlimited height, soft crop
+
+  // Register custom image size for image post formats.
+  add_image_size( 'sempress-image-post', 900, 1288 );
+
   // remove some theme supports from SemPress
   remove_theme_support( 'custom-background' );
   remove_theme_support( 'custom-header' );
