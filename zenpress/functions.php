1ae12151f734736de5eff3ddfb0edc2f02328ad1
Matthias Pfefferle <pfefferle@gmail.com>
2017-02-13 22:29:16 +0100

    some css tweaks

--- zenpress/functions.php
+++ zenpress/functions.php
@@ -226,18 +226,16 @@ if ( ! function_exists( 'zenpress_content_nav' ) ) :
 	 */
 	function zenpress_content_nav( $nav_id ) {
 		global $wp_query;
-
 		?>
+		<?php if ( is_single() ) : // navigation links for single posts ?>
 		<nav id="<?php echo $nav_id; ?>">
 			<h1 class="assistive-text section-heading"><?php _e( 'Post navigation', 'zenpress' ); ?></h1>
-
-		<?php if ( is_single() ) : // navigation links for single posts ?>
-
 			<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'zenpress' ) . '</span> %title' ); ?>
 			<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'zenpress' ) . '</span>' ); ?>
-
+		</nav><!-- #<?php echo $nav_id; ?> -->
 		<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
-
+		<nav id="<?php echo $nav_id; ?>">
+			<h1 class="assistive-text section-heading"><?php _e( 'Post navigation', 'zenpress' ); ?></h1>
 			<?php if ( get_next_posts_link() ) : ?>
 			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'zenpress' ) ); ?></div>
 			<?php endif; ?>
@@ -245,10 +243,8 @@ if ( ! function_exists( 'zenpress_content_nav' ) ) :
 			<?php if ( get_previous_posts_link() ) : ?>
 			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'zenpress' ) ); ?></div>
 			<?php endif; ?>
-
-		<?php endif; ?>
-
 		</nav><!-- #<?php echo $nav_id; ?> -->
+		<?php endif; ?>
 		<?php
 	}
 endif; // zenpress_content_nav
