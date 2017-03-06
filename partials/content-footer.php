cf8ae2ef709174e6e7c0e4c32bcc87a59a11d6a3
Matthias Pfefferle <pfefferle@gmail.com>
2017-03-06 22:59:23 +0100

    fixed author

--- partials/content-footer.php
+++ partials/content-footer.php
@@ -3,9 +3,9 @@
 		<address class="author p-author vcard hcard h-card" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
 			<?php echo get_avatar( get_the_author_meta( 'ID' ), 100 ); ?>
 			<a class="url uid u-url u-uid fn p-name" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
-				<?php echo get_the_author(); ?>
+				<span itemprop="name"><?php echo get_the_author(); ?></span>
 			</a>
-			<div class="note e-note"><?php echo get_the_author_meta( 'description' ); ?></div>
+			<div class="note e-note" itemprop="description"><?php echo get_the_author_meta( 'description' ); ?></div>
 		</address>
 
 		<?php
