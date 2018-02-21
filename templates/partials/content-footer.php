6e2a6e0b144a61f3c2359455e46128025c84be26
Matthias Pfefferle <pfefferle@gmail.com>
2018-02-21 20:54:54 +0100

    fixed feed link

--- templates/partials/content-footer.php
+++ templates/partials/content-footer.php
@@ -6,7 +6,7 @@
 				<span itemprop="name"><?php echo get_the_author(); ?></span>
 			</a>
 			<div class="note e-note" itemprop="description"><?php echo get_the_author_meta( 'description' ); ?></div>
-			<a class="subscribe" href="<?php echo get_feed_link(); ?>"><i class="openwebicons-feed"></i> <?php _e( 'Subscribe to author feed', 'zenpress' ); ?></a>
+			<a class="subscribe" href="<?php echo get_author_feed_link( get_the_author_meta( 'ID' ) ); ?>"><i class="openwebicons-feed"></i> <?php _e( 'Subscribe to author feed', 'zenpress' ); ?></a>
 		</address>
 
 		<?php
