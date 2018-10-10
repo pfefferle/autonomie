e258373b4d2beb3257c271b4302728e428a474e0
Ruxton <ruxton@rhythmandpoetry.net>
2018-10-10 11:44:48 +0800

    adds a cleaner video view more like insta

---
+++ includes/video.php
@@ -0,0 +1,49 @@
+<?php
+function zenpress_video_shortcode( $output, $atts, $video, $post_id, $library ) {
+	global $content_width;
+	static $instance = 0;
+  $instance++;
+
+	$html_atts = array(
+		'class'    => str_replace('wp-video-shortcode','',$atts['class']),
+		'id'       => sprintf( 'video-%d-%d', $post_id, $instance ),
+		'width'    => absint( $atts['width'] ),
+		'height'   => absint( $atts['height'] ),
+		'poster'   => esc_url( $atts['poster'] ),
+		'loop'     => wp_validate_boolean( $atts['loop'] ),
+		'autoplay' => wp_validate_boolean( $atts['autoplay'] ),
+		'preload'  => $atts['preload'],
+	);
+
+	// These ones should just be omitted altogether if they are blank
+	foreach ( array( 'poster', 'loop', 'autoplay', 'preload' ) as $a ) {
+			if ( empty( $html_atts[$a] ) ) {
+					unset( $html_atts[$a] );
+			}
+	}
+
+	$attr_strings = array();
+	foreach ( $html_atts as $k => $v ) {
+			$attr_strings[] = $k . '="' . esc_attr( $v ) . '"';
+	}
+
+?>
+<div class="video-wrapper">
+    <video width="<?php echo $content_width; ?>" <?php echo join( ' ', $attr_strings )." " ?>controls>
+        <source src="<?php echo $atts['src']; ?>"></source>
+        Your browser does not support the <code>video</code> tag.
+    </video>
+    <div class="video-controls">
+        <button data-media="play-pause"></button>
+        <button data-media="mute-unmute"></button>
+    </div>
+</div>
+<?php
+}
+
+function zenpress_enqueue_video_js() {
+  wp_enqueue_script('zenpress-video',get_template_directory_uri().'/js/video.js');
+}
+
+add_filter( 'wp_video_shortcode', 'zenpress_video_shortcode', 10, 5 );
+add_action( 'wp_enqueue_scripts', 'zenpress_enqueue_video_js');
---
+++ js/video.js
@@ -0,0 +1,101 @@
+'use strict';
+
+var SimpleVideo = function (el) {
+  	this.$video    = $(el);
+  	this.$wrapper  = $(el).parent().addClass('paused');
+  	this.$controls = this.$wrapper.find('.video-controls');
+
+  	// remove native controls
+  	this.$video.removeAttr('controls');
+
+  	// check if video should autoplay
+    if(!!this.$video.attr('autoplay')) {
+    		this.$wrapper.removeClass('paused').addClass('playing');
+    }
+
+  	// check if video is muted
+    if(this.$video.attr('muted') === 'true' || this.$video[0].volume === 0) {
+        this.$video[0].muted = true;
+        this.$wrapper.addClass('muted');
+    }
+
+  	// attach event handlers
+  	this.attachEvents();
+};
+
+SimpleVideo.prototype.attachEvents = function () {
+
+  	var self = this,
+        _t; // keep track of timeout for controls
+
+  	// attach handlers to data attributes
+    this.$wrapper.on('click', '[data-media]', function () {
+
+        var data = $(this).data('media');
+
+        if(data === 'play-pause') {
+            self.playPause();
+        }
+        if(data === 'mute-unmute') {
+            self.muteUnmute();
+        }
+    });
+
+  	this.$video.on('click', function () {
+    		self.playPause();
+    });
+
+    this.$video.on('play', function () {
+    		self.$wrapper.removeClass('paused').addClass('playing');
+    });
+
+    this.$video.on('pause', function () {
+    		self.$wrapper.removeClass('playing').addClass('paused');
+    });
+
+    this.$video.on('volumechange', function () {
+        if($(this)[0].muted) {
+        		self.$wrapper.addClass('muted');
+        }
+        else {
+        		self.$wrapper.removeClass('muted');
+        }
+    });
+
+    this.$wrapper.on('mousemove', function () {
+
+        // show controls
+        self.$controls.addClass('video-controls--show');
+
+        // clear original timeout
+        clearTimeout(_t);
+
+        // start a new one to hide controls after specified time
+        _t = setTimeout(function () {
+            self.$controls.removeClass('video-controls--show');
+        }, 2250);
+
+    }).on('mouseleave', function () {
+        self.$controls.removeClass('video-controls--show');
+    });
+};
+
+SimpleVideo.prototype.playPause = function () {
+    if (this.$video[0].paused) {
+    		this.$video[0].play();
+    } else {
+		    this.$video[0].pause();
+    }
+};
+
+SimpleVideo.prototype.muteUnmute = function () {
+    if(this.$video[0].muted === false) {
+    		this.$video[0].muted = true;
+    } else {
+		    this.$video[0].muted = false;
+    }
+};
+
+$('video').each(function () {
+  	new SimpleVideo(this);
+});
