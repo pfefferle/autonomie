12cf04afb46670406541e4e4dd9f5446676343c3
Matthias Pfefferle <pfefferle@gmail.com>
2019-01-19 22:51:33 +0100

    encapsulate banner code

--- header.php
+++ header.php
@@ -51,18 +51,5 @@
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 
-		<?php if ( zenpress_show_page_banner() ) : ?>
-		<div class="page-banner">
-			<?php if ( ! is_singular() ) : ?>
-			<div class="page-branding">
-				<?php if ( zenpress_get_the_archive_title() ) { ?>
-				<h1 id="page-title"<?php zenpress_semantics( 'page-title' ); ?>><?php echo zenpress_get_the_archive_title(); ?></h1>
-				<?php } ?>
-				<?php if ( zenpress_get_the_archive_description() ) { ?>
-				<div id="page-description"<?php zenpress_semantics( 'page-description' ); ?>><?php echo zenpress_get_the_archive_description(); ?></div>
-				<?php } ?>
-			</div>
-			<?php endif; ?>
-		</div>
-		<?php endif; ?>
+		<?php get_template_part( 'templates/partials/page-banner' ); ?>
 	</header><!-- #site-header -->
