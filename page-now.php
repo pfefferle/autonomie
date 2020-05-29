939d7346c0f5192de3163981b5ad03ec47571a71
Matthias Pfefferle <pfefferle@gmail.com>
2020-05-29 09:46:26 +0200

    /now Template
    see: https://christiantietze.de/posts/2020/05/now-page-microformat/ by @ChristianTietze

---
+++ page-now.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: "/now" Template
+ *
+ * The template for displaying "/now" pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package Autonomie
+ * @since Autonomie 1.0.0
+ */
+
+get_header(); ?>
+
+		<main id="primary" <?php autonomie_main_class( 'h-news' ); ?><?php autonomie_semantics( 'main' ); ?>>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'templates/content', 'single' ); ?>
+
+				<?php comments_template( '', true ); ?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #content -->
+
+<?php get_footer(); ?>
