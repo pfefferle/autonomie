a2532178699fcf1964436f656aa57ea1eef159a1
Matthias Pfefferle <pfefferle@gmail.com>
2020-06-25 17:48:57 +0200

    webaction: reply

--- template-parts/entry-footer.php
+++ template-parts/entry-footer.php
@@ -32,14 +32,14 @@
 		<?php endif; // End if $tags_list ?>
 
 		<?php dynamic_sidebar( 'entry-meta' ); ?>
-		<?php do_action( 'autonomie-entry-footer' ); ?>
+		<?php do_action( 'autonomie_entry_footer' ); ?>
 
 		<?php // get_template_part( 'template-parts/entry', 'nav' ); ?>
 	</footer><!-- #entry-meta -->
 <?php else : ?>
 	<footer class="entry-footer entry-meta">
-		<?php if ( comments_open() || ( '0' != get_comments_number() && ! comments_open() ) ) : ?>
-		<div class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'autonomie' ), __( '1 Comment', 'autonomie' ), __( '% Comments', 'autonomie' ) ); ?></div>
+		<?php if ( comments_open() || ( '0' !== get_comments_number() && ! comments_open() ) ) : ?>
+		<indie-action do="reply" width="<?php the_permalink(); ?>"><div class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'autonomie' ), __( '1 Comment', 'autonomie' ), __( '% Comments', 'autonomie' ) ); ?></div></indie-action>
 		<?php endif; ?>
 	</footer><!-- #entry-meta -->
 <?php endif; ?>
