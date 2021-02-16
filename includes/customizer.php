a53359af3b7253781b471023dad88a08cb4f24c7
Matthias Pfefferle <pfefferle@gmail.com>
2021-02-16 10:30:48 +0100

    remove "pretty feed" feature
    please use https://github.com/pfefferle/wordpress-pretty-feeds instead

--- includes/customizer.php
+++ includes/customizer.php
@@ -10,29 +10,9 @@ function autonomie_customize_register( $wp_customize ) {
 		'autonomie_settings_section',
 		array(
 			'title' => __( 'Advanced Settings', 'autonomie' ),
-			'description' => __( 'Enable/Discable some advanced Autonomie features.', 'autonomie' ), //Descriptive tooltip
+			'description' => __( 'Enable/Disable some advanced Autonomie features.', 'autonomie' ), //Descriptive tooltip
 			'priority' => 35,
 		)
 	);
-
-	$wp_customize->add_setting(
-		'autonomie_feed_templates',
-		array(
-			'default' => '0',
-			'transport' => 'refresh',
-			'sanitize_callback' => 'sanitize_key',
-		)
-	);
-
-	$wp_customize->add_control(
-		'autonomie_feed_templates',
-		array(
-			'label' => __( 'Feed Templates', 'autonomie' ),
-			'description' => __( 'Enable/Discable XSL Templates for Feeds, similar to the old Feedburner Templates.', 'autonomie' ),
-			'section' => 'autonomie_settings_section',
-			'settings' => 'autonomie_feed_templates',
-			'type' => 'checkbox',
-		)
-	);
 }
-add_action( 'customize_register', 'autonomie_customize_register' );
+// add_action( 'customize_register', 'autonomie_customize_register' );
