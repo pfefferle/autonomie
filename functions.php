8324f479c00ccce800e515ad068a4e89b0865228
Ali Reza Hayati <64958513+arhayati@users.noreply.github.com>
2022-01-17 08:10:29 +0000

    Added automatic html id to headings

--- functions.php
+++ functions.php
@@ -332,6 +332,23 @@ function autonomie_content_width() {
 add_action( 'after_setup_theme', 'autonomie_content_width', 0 );
 
 /**
+ * Automatically add IDs to headings such as <h2></h2>
+ */
+function auto_id_headings( $content ) {
+
+	$content = preg_replace_callback( '/(\<h[1-6](.*?))\>(.*)(<\/h[1-6]>)/i', function( $matches ) {
+		if ( ! stripos( $matches[0], 'id=' ) ) :
+			$matches[0] = $matches[1] . $matches[2] . ' id="' . sanitize_title( $matches[3] ) . '">' . $matches[3] . $matches[4];
+		endif;
+		return $matches[0];
+	}, $content );
+
+    return $content;
+
+}
+add_filter( 'the_content', 'auto_id_headings' );
+
+/**
  * Set the default maxwith for the embeds
  */
 function autonomie_embed_defaults() {
